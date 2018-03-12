e4e63cac631f434d7d838753eeea4148f97157f7
DESKTOP-1VSR9F7\John Greaves <jsg339@drexel.edu>
2018-03-12 14:28:29 -0400

    Test

---
+++ Game/Undecied Major/Assets/Standard Assets/2D/Scripts/EnemyScirpt.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScirpt : MonoBehaviour
+{
+    public LayerMask enemyMask;
+    public Transform target;
+    public float speed;
+    Rigidbody2D myBody;
+    Transform myTrans;
+    float myWidth;
+
+    private void Start()
+    {
+        myTrans = this.transform;
+        myBody = this.GetComponent<Rigidbody2D>();
+        myWidth = this.GetComponent<SpriteRenderer>().bounds.extents.x;
+    }
+    private void Update()
+    {
+        if (target.position.x >= 7.3)
+        {
+            float movementDistance = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, movementDistance);
+        }
+        if (target.GetComponent<BoxCollider2D>().IsTouching(myTrans.GetComponent<BoxCollider2D>()))
+        {
+            PlayerHealth.takeDamage(0.2);
+            //Play animation here
+        }
+    }
+}
---
+++ Game/Undecied Major/Assets/Standard Assets/2D/Scripts/PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class PlayerHealth : MonoBehaviour
+{
+    public const double MAX_GPA = 4.0;
+    public static double currentGpa = MAX_GPA;
+	
+
+    public static void takeDamage(double damage)
+    {
+        currentGpa -= damage;
+        if (currentGpa <= 0.0)
+        {
+            currentGpa = MAX_GPA;
+            print("You died!");
+            SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+        }
+
+    }
+}
