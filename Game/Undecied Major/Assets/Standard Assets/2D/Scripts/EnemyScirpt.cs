05f4fa0813f46b6f9b70ae631c22275292b076f3
DESKTOP-1VSR9F7\John Greaves <jsg339@drexel.edu>
2018-03-15 01:11:08 -0400

    Finished Prototype

--- Game/Undecied Major/Assets/Standard Assets/2D/Scripts/EnemyScirpt.cs
+++ Game/Undecied Major/Assets/Standard Assets/2D/Scripts/EnemyScirpt.cs
@@ -4,33 +4,33 @@ using UnityEngine;
 
 public class EnemyScirpt : MonoBehaviour
 {
-    public LayerMask enemyMask;
     public Transform target;
     public float speed;
-    Rigidbody2D myBody;
-    Transform myTrans;
-    float myWidth;
+    private Rigidbody2D myBody;
+    private Transform myTrans;
 	static float currentHealth;
 	public float maxHealth;
+    private bool moving = false;
+    PlayerHealth playerHp;
 
     private void Start()
     {
 		currentHealth = maxHealth;
         myTrans = this.transform;
         myBody = this.GetComponent<Rigidbody2D>();
-        myWidth = this.GetComponent<SpriteRenderer>().bounds.extents.x;
+        playerHp = target.GetComponent<PlayerHealth>();
     }
     private void Update()
     {
-        if (target.position.x >= 7.3)
+        if ((target.position.x >= 7.3 ) && (target.position.y <= 0.6) || moving)
         {
             float movementDistance = speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, target.position, movementDistance);
+            moving = true;
         }
         if (target.GetComponent<BoxCollider2D>().IsTouching(myTrans.GetComponent<BoxCollider2D>()))
         {
-            PlayerHealth.takeDamage(0.2);
-            //Play animation here
+            playerHp.takeDamage(0.2);
         }
     }
 	public static void takeDamage(float damage)
--- Game/Undecied Major/Assets/Standard Assets/2D/Scripts/PlayerAttack.cs
+++ Game/Undecied Major/Assets/Standard Assets/2D/Scripts/PlayerAttack.cs
@@ -7,21 +7,28 @@ public class PlayerAttack : MonoBehaviour
 	public float attackDamage;
 	public Transform target;
 	Transform myTrans;
+    Animator playerAni;
 
 	public void Start()
 	{
 		myTrans = this.transform;
+        playerAni = GetComponent<Animator>();
 	}
 	public void Update () 
 	{
 		if(Input.GetKeyDown(KeyCode.P))
 		{
-			//animate attack
-			if(target.position.x - myTrans.position.x >= 0.5 && target.position.x - myTrans.position.x < 1)
-			{
-				print("Hit");
+            playerAni.Play("Melee");
+            if (((target.position.x - myTrans.position.x >= 0.5 && target.position.x - myTrans.position.x < 1) || 
+               (myTrans.position.x - target.position.x >= 0.5 && myTrans.position.x - target.position.x < 1)) && 
+                myTrans.position.y <= 0.6)
+            {
 				EnemyScirpt.takeDamage(attackDamage);
 			}
 		}
+        if(Input.GetKeyUp(KeyCode.P))
+        {
+            playerAni.Play("Idle");
+        }
 	}
 }
