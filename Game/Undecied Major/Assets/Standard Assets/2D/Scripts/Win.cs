cba3c307a1aca621318987ef3528df3ce0bbd1d7
DESKTOP-1VSR9F7\John Greaves <jsg339@drexel.edu>
2018-03-17 12:30:31 -0400

    merge

---
+++ Game/Undecied Major/Assets/Standard Assets/2D/Scripts/Win.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Win : MonoBehaviour
+{
+
+    void OnGUI()
+    {
+        GUI.TextArea(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 150, 100, 40), "GPA:" + GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>().currentGpa);
+        //if retry button is pressed load scene 0 the game
+        GUI.contentColor = Color.red;
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 50, 100, 40), "Retry?"))
+        {
+            Application.LoadLevel(0);
+        }
+        //and quit button
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 100, 100, 40), "Quit"))
+        {
+            Application.Quit();
+        }
+    }
+
+}
